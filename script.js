f7bf2f85ad739f048f01a0009b146eba31fcdda6
Kevoki <kokioma@gmail.com>
2019-12-02 12:40:41 +0300

    Adding variables on javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+var century;
+var year;
+var month;
+var day;
+//get inout
+
+function getInput(){
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfMonth = parseInt(document.getElementById("monthday").value);
+
+  
